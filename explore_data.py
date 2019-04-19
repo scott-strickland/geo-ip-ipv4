b68a9674c9bd35f209a542b281ad4c7bd95d3403
scott-strickland <sstrickland@sycomtech.com>
2019-04-19 12:37:43 -0400

    Putting into base functions.

--- explore_data.py
+++ explore_data.py
@@ -5,27 +5,33 @@ import netaddr
 This was written in Python 3.7.2.  The purpose of this file is to explore the dataset of IPv4 geolocation from https://datahub.io/collections/reference-data#ipv4-geolocation.
 """
 
-infile = 'geo-ipv4.json'
-outfile = 'agg-ipv4.json'
-cidr_list = []
-net_list = []
+
+def load_data():
+    """Load the raw geo IPv4 data from JSON."""
+    with open(infile, 'r') as f:
+        data = json.load(f)
+    for item in data:
+        for k, v in item.items():
+            if k == 'network':
+                cidr_list.append(v)
+    return cidr_list
 
 
-with open(infile, 'r') as f:
-    data = json.load(f)
+def write_data(cidr_list):
+    """Write the extracted data to a new JSON file."""
+    cidr_range = netaddr.IPSet(cidr_list)
+    with open(outfile, 'w') as f:
+        for cidr in cidr_range.iter_cidrs():
+            net_list.append(str(cidr))
+        json.dump(net_list, f, indent=2)
 
-for item in data:
-    for k, v in item.items():
-        if k == 'network':
-            cidr_list.append(v)
 
+if __name__ == '__main__':
+    infile = 'data/geo-ipv4.json'
+    outfile = 'data/agg-ipv4.json'
+    cidr_list = []
+    net_list = []
+    load_data()
+    write_data(cidr_list)
 
-cidr_range = netaddr.IPSet(cidr_list)
-#net_list = json.dumps(cidr_range)
-
-with open(outfile, 'w') as f:
-    for cidr in cidr_range.iter_cidrs():
-        net_list.append(str(cidr))
-#        net_list = json.loads(net_list)
-    json.dump(net_list, f, indent=2)
     
